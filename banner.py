a17540877e5326c1847b4b7318b04f440ae88e01
chsafe <3685068@gmail.com>
2017-06-27 23:59:37 +0800

    Update banner.py

--- banner.py
+++ banner.py
@@ -3,7 +3,7 @@ import pexpect
 
 def banner(ip):
     child = pexpect.spawn('telnet %s' % ip)
-    index = child.expect (['ame', 'nter','ccount',pexpect.TIMEOUT,pexpect.EOF],timeout=3) 
+    index = child.expect (['ame', 'nter','ccount','ogin',pexpect.TIMEOUT,pexpect.EOF],timeout=3) 
     if index > 2 :  
         print ip + ":" +"someting is wrong"
     else:
